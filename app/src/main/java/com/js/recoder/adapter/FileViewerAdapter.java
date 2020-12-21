52125db814dce980c20550cc42784d73d66f20a0
Home <nontile@daum.net>
2020-12-21 20:50:10 +0900

    Update FileViewerAdapter

---
+++ app/src/main/java/com/js/recoder/adapter/FileViewerAdapter.java
@@ -0,0 +1,190 @@
+package com.js.recoder.adapter;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.js.recoder.DBHelper;
+import com.js.recoder.R;
+import com.js.recoder.RecordingItem;
+import com.js.recoder.listeners.OnDataBaseChangedListener;
+
+import java.io.File;
+
+public class FileViewerAdapter extends RecyclerView.Adapter<FileViewerAdapter.RecordingsViewHolder>
+implements OnDataBaseChangedListener{
+
+    private DBHelper mDatabase;
+    private static final String LOG_TAG = "FileViewerAdapter";
+    RecordingItem item;
+    Context mContext;
+    LinearLayoutManager llm;
+
+    public FileViewerAdapter(Context context, LinearLayoutManager linearLayoutManager){
+        super();
+        mContext = context;
+        mDatabase = new DBHelper(mContext);
+        mDatabase.setOnDatabaseChangedListener(this);
+        llm = linearLayoutManager;
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull RecordingsViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public RecordingsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.
+                from(parent.getContext()).
+                inflate(R.layout.card_view, parent, false);
+
+        mContext = parent.getContext();
+
+        return new RecordingsViewHolder(itemView);
+    }
+
+    public static class RecordingsViewHolder extends RecyclerView.ViewHolder {
+        protected TextView vName;
+        protected TextView vLength;
+        protected TextView vDateAdded;
+        protected View cardView;
+
+        public RecordingsViewHolder(View v) {
+            super(v);
+            vName = (TextView) v.findViewById(R.id.file_name_text);
+            vLength = (TextView) v.findViewById(R.id.file_length_text);
+            vDateAdded = (TextView) v.findViewById(R.id.file_date_added_text);
+            cardView = v.findViewById(R.id.card_view);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatabase.getCount();
+    }
+
+    public RecordingItem getItem(int position) {
+        return mDatabase.getItemAt(position);
+    }
+
+    @Override
+    public void onNewDatabaseEntryAdded() {
+        //item added to top of the list
+        notifyItemInserted(getItemCount() - 1);
+        llm.scrollToPosition(getItemCount() - 1);
+    }
+
+    @Override
+    public void onDatabaseEntryRenamed() {
+
+    }
+
+
+    public void remove(int position) {
+        //remove item from database, recyclerview and storage
+
+        //delete file from storage
+        File file = new File(getItem(position).getFilePath());
+        file.delete();
+
+        Toast.makeText(
+                mContext,
+                String.format(
+                        mContext.getString(R.string.toast_file_delete),
+                        getItem(position).getName()
+                ),
+                Toast.LENGTH_SHORT
+        ).show();
+
+        mDatabase.removeItemWithId(getItem(position).getId());
+        notifyItemRemoved(position);
+    }
+
+    //TODO
+    public void removeOutOfApp(String filePath) {
+        //user deletes a saved recording out of the application through another application
+    }
+
+    public void rename(int position, String name) {
+        //rename a file
+
+        String mFilePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFilePath += "/SoundRecorder/" + name;
+        File f = new File(mFilePath);
+
+        if (f.exists() && !f.isDirectory()) {
+            //file name is not unique, cannot rename file.
+            Toast.makeText(mContext,
+                    String.format(mContext.getString(R.string.toast_file_exists), name),
+                    Toast.LENGTH_SHORT).show();
+
+        } else {
+            //file name is unique, rename file
+            File oldFilePath = new File(getItem(position).getFilePath());
+            oldFilePath.renameTo(f);
+            mDatabase.renameItem(getItem(position), name, mFilePath);
+            notifyItemChanged(position);
+        }
+    }
+
+    public void shareFileDialog(int position) {
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(getItem(position).getFilePath())));
+        shareIntent.setType("audio/mp4");
+        mContext.startActivity(Intent.createChooser(shareIntent, mContext.getText(R.string.send_to)));
+    }
+
+    public void renameFileDialog (final int position) {
+        // File rename dialog
+        AlertDialog.Builder renameFileBuilder = new AlertDialog.Builder(mContext);
+
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View view = inflater.inflate(R.layout.dialog_rename_file, null);
+
+        final EditText input = (EditText) view.findViewById(R.id.new_name);
+
+        renameFileBuilder.setTitle(mContext.getString(R.string.dialog_title_rename));
+        renameFileBuilder.setCancelable(true);
+        renameFileBuilder.setPositiveButton(mContext.getString(R.string.dialog_action_ok),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        try {
+                            String value = input.getText().toString().trim() + ".mp4";
+                            rename(position, value);
+
+                        } catch (Exception e) {
+                            Log.e(LOG_TAG, "exception", e);
+                        }
+
+                        dialog.cancel();
+                    }
+                });
+        renameFileBuilder.setNegativeButton(mContext.getString(R.string.dialog_action_cancel),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        renameFileBuilder.setView(view);
+        AlertDialog alert = renameFileBuilder.create();
+        alert.show();
+    }
+}
